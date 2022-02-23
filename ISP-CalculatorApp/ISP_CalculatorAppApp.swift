1cd110c736724dadab51e00a5107e5971062c352
DulithT <duliththaveesha@gmail.com>
2022-02-22 20:33:26 -0500

    Initial Commit

---
+++ ISP-CalculatorApp/ISP_CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISP_CalculatorAppApp.swift
+//  ISP-CalculatorApp
+//
+//  Created by Dulith Thaveesha on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct ISP_CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
